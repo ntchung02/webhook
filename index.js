bb16c1548473b0212f60a5d0a142b80adcff85b3
ntchung02 <163388040+ntchung02@users.noreply.github.com>
2025-06-13 12:16:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,39 +27,6 @@ app.listen(PORT, () => {
 });
 
 
-app.post('/test', async (req, res) => {
-  try {
-    const payload = {
-      partnerCode: "MOMORKPA20240709_TEST",
-      accessKey: "mjHEUVjfCewyaKbP",
-      requestId: "test123456789",
-      amount: "10000",
-      orderId: "order123456789",
-      orderInfo: "Test kết nối",
-      redirectUrl: "https://google.com",
-      ipnUrl: "https://webhook.site/test",
-      requestType: "captureWallet",
-      extraData: "",
-      lang: "vi",
-      signature: "signature-test" // Dùng tạm hoặc tạo đúng nếu cần
-    };
-
-    const response = await axios.post('https://payment.momo.vn/v2/gateway/api/create', payload, {
-      headers: { 'Content-Type': 'application/json' }
-    });
-
-    res.status(200).json({
-      success: true,
-      momo_response: response.data
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-      detail: err.response?.data || null
-    });
-  }
-});
 
 // /relay-momo/v2/gateway/api/create
 app.post('/relay-momo/v2/gateway/api/create', async (req, res) => {
@@ -67,7 +34,7 @@ app.post('/relay-momo/v2/gateway/api/create', async (req, res) => {
     const axios = require('axios');
 
     const response = await axios.post(
-      'https://test-payment.momo.vn/v2/gateway/api/create',
+      'https://payment.momo.vn/v2/gateway/api/create',
       req.body,
       {
         headers: {
